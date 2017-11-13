5fc5a31d741f3aa444d8d0da85c36b068cc4260d
Oleg Suzdalev <OlegSouzdalev@users.noreply.github.com>
2017-11-13 18:12:38 +0300

    Update SFE.py

--- SFE.py
+++ SFE.py
@@ -1,18 +1,9 @@
-#SFE compression algo
-
 from math import log2
 import random as r
 
 class SFE:
-    def __init__(self, input):
-        self.input = input
+    def __init__(self):
         self.byte_size = 8
-        self.bytes = []
-        self.table = None
-        self.dictionary = None
-        self.coded = []
-        self.decoded = []
-
 
     # converts float to binary
     def float2bin(self, x, bit='0.'):
@@ -33,72 +24,77 @@ class SFE:
             return float(bit)
 
     # void input –> array of byte converted to string
-    def getbytes(self):
+    def getbytes(self, input):
+        bytes = []
         # filling list 'bytes'
-        for i in range(0, len(self.input), self.byte_size):
+        for i in range(0, len(input), self.byte_size):
             byte = ''
             for j in range(i, i + self.byte_size):
-                byte += str(self.input[j])
-            self.bytes.append(byte)
+                byte += str(input[j])
+            bytes.append(byte)
+
+        return bytes
 
     # returns a list of lists [[bytes], [probabilities]]
-    def gettable(self):
+    def gettable(self, input):
         # activating previous step
-        self.getbytes()
+        bytes = self.getbytes(input)
         # # of bytes
-        total = len(self.input) / self.byte_size
+        total = len(input) / self.byte_size
         # creating dictionary out of 'bytes'
         reference = dict()
-        for byte in self.bytes:
+        for byte in bytes:
             reference[byte] = reference.get(byte, 0) + 1
 
         # Since dictionaries cannot be sorted, we are creating a sorted list representation instead
         # sorting array by decreasing frequency
         sort = sorted(reference, key=reference.get, reverse=True)
         # in 2nd array values are replaced by byte probability (value / total)
-        self.table = [[k for k in sort], [float(reference[k] / total) for k in sort]]
+        return [[k for k in sort], [float(reference[k] / total) for k in sort]]
 
-    # returns a list of lists [[byte:str], [code:str]]
-    def getcode(self):
+    # returns a dictionary {byte(str) : code(str)}
+    def getdictionary(self, input):
         # activating previous step
-        self.gettable()
+        table = self.gettable(input)
 
         codes = []
 
-        for i in range(len(self.table[1])):
+        for i in range(len(table[1])):
             # sum of previous probabilities
-            prev_prob = sum(self.table[1][:i])
+            prev_prob = sum(table[1][:i])
             # finding # of most significant bits to pick from binary float
-            sig_bits = int(log2(1 / self.table[1][i])) + 1
+            sig_bits = int(log2(1 / table[1][i])) + 1
             # converting function value to string of binaries
-            binary = '{:.16f}'.format(self.float2bin(prev_prob + self.table[1][i] * 0.5)).split('.')[1]
+            binary = '{:.16f}'.format(self.float2bin(prev_prob + table[1][i] * 0.5)).split('.')[1]
             # append code to list
             codes.append(binary[:sig_bits])
 
-        self.dictionary = dict(zip(self.table[0], codes))
+        return dict(zip(table[0], codes))
 
     # returns input compressed
-    def encode(self):
-        # activating previous step
-        self.getcode()
+    def encode(self, input):
+        coded = []
+        bytes = self.getbytes(input)
+        dictionary = self.getdictionary(input)
 
-        for i in range(len(self.bytes)):
-            code = list(self.dictionary.get(self.bytes[i]))
+        for i in range(len(bytes)):
+            code = list(dictionary.get(bytes[i]))
             for i in range(len(code)):
-                self.coded.append(int(code[i]))
+                coded.append(int(code[i]))
 
-        return self.coded
+        return coded
 
-    def decode(self):
+    def decode(self, input, dictionary):
+        decoded = []
         code = ''
 
-        for i in range(len(self.coded)):
-            code += str(self.coded[i])
-            for key, value in self.dictionary.items():
+        for i in range(len(input)):
+            code += str(input[i])
+            for key, value in dictionary.items():
                 if value == code:
                     code = list(key)
                     for i in range(len(code)):
-                        self.decoded.append(int(code[i]))
+                        decoded.append(int(code[i]))
                     code = ''
 
-        return self.decoded
+        return decoded
