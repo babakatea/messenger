100bce52a352fd81e9b9c94532daaf7bd703d8ba
Oleg Suzdalev <OlegSouzdalev@users.noreply.github.com>
2017-11-13 18:13:38 +0300

    Update repetition.py

--- repetition.py
+++ repetition.py
@@ -1,22 +1,16 @@
-# Repetition encoding/decoding algo
-
-
 class Repetition:
-    def __init__(self, input):
-        self.input = input
+    def __init__(self):
         self.repetition = 3
-        self.coded = []
-        self.decoded = []
 
     # returns encode input
-    def encode(self):
-
-        for i in range(len(self.input)):
+    def encode(self, input):
+        coded = []
+        for i in range(len(input)):
             # repeat each bit 'repetition' # of times and add to array
             for j in range(self.repetition):
-                self.coded.append(self.input[i])
+                coded.append(input[i])
 
-        return self.coded
+        return coded
 
     # returns most common bit in array
     def most_common(self, array):
@@ -31,10 +25,9 @@ class Repetition:
         return 0 if zero > one else 1
 
     # returns decoded array
-    def decode(self):
+    def decode(self, input):
+        decoded = []
         # coded array is divided into sub-arrays of size 'repetition'
-        for i in range(0, len(self.coded), self.repetition):
+        for i in range(0, len(input), self.repetition):
             # sub-arrays are sent to function 'most_common', the returned bit is appended to 'decoded' array
-            self.decoded.append(self.most_common(self.coded[i:i + self.repetition]))
-
-        return self.decoded
+            decoded.append(self.most_common(input[i:i + self.repetition]))
